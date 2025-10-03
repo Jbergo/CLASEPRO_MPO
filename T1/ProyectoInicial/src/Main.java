82d9eea9571ef90d9a482a2777bb6b226423425e
Jorge <99258565+Jbergo@users.noreply.github.com>
2025-10-03 13:01:33 +0200

    update

---
+++ T1/ProyectoInicial/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    //Método Main -> obligatorio en todos los programas
+    public static void main(String[] args) {
+        System.out.print("Este es mi primer programa en Java\n");
+        System.out.println("Este es la segunda línea del programa");
+    }
+
+    public void nombreMetodo(){
+
+    }
+}
