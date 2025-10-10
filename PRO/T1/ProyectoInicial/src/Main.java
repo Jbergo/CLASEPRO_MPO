3b1c83b5dc376443d249a45cba0a5de0395ae240
Jorge Bermudez <a.jbergo31@gmail.com>
2025-10-10 12:59:27 +0200

    update

--- PRO/T1/ProyectoInicial/src/Main.java
+++ PRO/T1/ProyectoInicial/src/Main.java
@@ -1,8 +1,31 @@
 public class Main {
     //Método Main -> obligatorio en todos los programas
     public static void main(String[] args) {
-        System.out.print("Este es mi primer programa en Java\n");
-        System.out.println("Este es la segunda línea del programa");
+        System.out.println("Primer programa en JAVA");
+
+        //TIPOS DE VARIABLES (variables complejas cuando empiezan en mayúscula, y variable primitiva cuando empiezan en minúscula)
+        String miNombre = "Jorge";
+        miNombre = "Jorge Bermúdez"; //Redefinir variables
+
+        char letraDNI = 'N';
+        Character letraCompleja = 'n';
+        letraDNI = 78;
+
+        int edad = 23;
+        Integer edadCompleja = 23;
+        edad++; //Incrementa un número
+
+        double altura = 1.71;
+        Double alturaCompleja = 21.76;
+
+        float peso = 70.76f;
+        Float pesoCompleja = 70.73f;
+
+        boolean experiencia = true;
+        Boolean experienciaCompleja = true;
+
+        final String DNI = "123123A";
+
     }
 
     public void nombreMetodo(){
---
+++ PRO/T1/ProyectoInicial/src/MainMetodos.java
@@ -0,0 +1,12 @@
+public class MainMetodos {
+    public static void main(String[] args) {
+        System.out.println("Main para probar métodos y variables");
+
+        saludar("Jorge");
+
+    }
+
+    public static void saludar(String data) {
+        System.out.println("Enhorabuena " + data + " has llamado a tu primer método");
+    }
+}
