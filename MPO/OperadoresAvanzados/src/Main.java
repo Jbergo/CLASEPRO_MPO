43c02e5f0dab7ecf12adcdd03620f2f6018b0a99
Jorge Bermudez <a.jbergo31@gmail.com>
2025-11-03 11:20:19 +0100

    update

--- MPO/OperadoresAvanzados/src/Main.java
+++ MPO/OperadoresAvanzados/src/Main.java
@@ -10,6 +10,24 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Iniciando la clase");
         GestorUsuarios gestorUsuarios = new GestorUsuarios();
-        gestorUsuarios.validarUsuario("jorge", 28, "jorge@gmail.com");
+
+        /*
+        COMPARACIÓN DE NÚMEROS A NIVEL DE BIT
+            & -> AND BIT
+            | -> OR BIT
+            Este tipo de comparación comprueba todos los elementos
+        */
+
+        int numero1 = 5;
+        int numero2 = 6;
+
+        int resultado = numero1 & numero2;
+
+        System.out.println("El resultado con & es: "+resultado);
+
+        int resultado2 = numero1 | numero2;
+
+        System.out.println("El resultado con | es: "+resultado2);
+
     }
 }
