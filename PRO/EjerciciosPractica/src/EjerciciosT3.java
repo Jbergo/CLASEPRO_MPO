7967cf8e6c063c14f25cc1dbede0dda0dc978ae7
Jorge Bermudez <a.jbergo31@gmail.com>
2025-11-06 16:51:40 +0100

    update

--- PRO/EjerciciosPractica/src/EjerciciosT3.java
+++ PRO/EjerciciosPractica/src/EjerciciosT3.java
@@ -262,4 +262,25 @@ public class EjerciciosT3 {
 
         sc.close();
     }
+
+    public void ejercicio12(){
+        /*
+        Crea un programa que pida al usuario crear una contraseña.
+        La contraseña debe cumplir estos requisitos: tener al menos
+        8 caracteres de longitud, contener al menos un número, y
+        NO puede ser "12345678" o "password". El programa debe
+        verificar cada condición por separado usando operadores
+        lógicos y mostrar si la contraseña es válida o no,
+        indicando qué requisitos no cumple.
+        Nota: Para este ejercicio, asume que tienes funciones
+        auxiliares como longitudCadena(texto), contienNumero(texto).
+        */
+
+        System.out.println("Crea una contraseña:");
+        String contraseña = sc.next();
+
+        if (contraseña.length()<8&&contraseña.contains()) {
+            System.out.println("Contraseña no válida");
+        } else if(contraseña)
+    }
 }
