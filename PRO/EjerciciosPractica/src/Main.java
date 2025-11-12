2ca15dbef6efb2fb5d859d31e29129132bea0f0b
Jorge Bermudez <a.jbergo31@gmail.com>
2025-11-12 14:18:13 +0100

    update

--- PRO/EjerciciosPractica/src/Main.java
+++ PRO/EjerciciosPractica/src/Main.java
@@ -2,11 +2,21 @@ public class Main {
     public static void main(String[] args) {
 
         /*EJERCICIOS TEMA 1*/
+//        EjerciciosT1 ejercicios = new EjerciciosT1();
 
         /*EJERCICIOS TEMA 2*/
+//        EjerciciosT2 ejercicios = new EjerciciosT2();
 
         /*EJERCICIOS TEMA 3*/
-        EjerciciosSwitchFor ejercicios = new EjerciciosSwitchFor();
-        ejercicios.ejercicio9();
+//        EjerciciosT3 ejercicios = new EjerciciosT3();
+
+
+        /*EJERCICIOS SWITCH Y FOR*/
+//        EjerciciosSwitchFor ejercicios = new EjerciciosSwitchFor();
+
+        /*EJERCICIOS WHILE Y DO-WHILE*/
+        EjerciciosWhileDoWhile ejercicios = new EjerciciosWhileDoWhile();
+        ejercicios.ejercicio10();
+
     }
 }
