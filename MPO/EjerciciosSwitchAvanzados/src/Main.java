7e95a62d04ddbb281855e769a816b7e2ff025671
Jorge Bermudez <a.jbergo31@gmail.com>
2025-11-27 12:54:34 +0100

    update

---
+++ MPO/EjerciciosSwitchAvanzados/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        EjerciciosSwitchAvanzado ejercicios = new EjerciciosSwitchAvanzado();
+        ejercicios.ejercicio1();
+    }
+}
