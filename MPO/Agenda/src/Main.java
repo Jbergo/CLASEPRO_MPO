6e212c4c71709097a2ec59e2ccc0459c59dc2e0c
Jorge <99258565+Jbergo@users.noreply.github.com>
2025-12-10 09:56:40 +0100

    update

--- MPO/Agenda/src/Main.java
+++ MPO/Agenda/src/Main.java
@@ -17,19 +17,21 @@ public class Main {
     static Scanner sc = new Scanner(System.in);
 
     //Crear listaUsuarios -> [nombre, apellido, telefono, dni]
-    static ArrayList<Object> listaUsuarios = new ArrayList<>();
+    static ArrayList<Object[]> listaUsuarios = new ArrayList<>();
 
     public static void main(String[] args) {
         int opcion;
 
         do {
             System.out.println("""
-                Elige la opción a realizar:
+                OPCIONES:
                 1. Agregar persona
                 2. Buscar persona
                 3. Borrar persona
                 4. Listar personas
                 5. Salir
+                
+                Elige la opción a realizar:
                 """);
             opcion = sc.nextInt();
 
@@ -38,10 +40,10 @@ public class Main {
                     agregarPersona(listaUsuarios);
                 }
                 case 2 -> {
-                    buscarPersona(listaUsuarios);
+
                 }
                 case 3 -> {
-                    borrarPersona(listaUsuarios);
+
                 }
                 case 4 -> {
                     listarPersonas(listaUsuarios);
@@ -56,44 +58,46 @@ public class Main {
         sc.close();
     }
 
-    private static void agregarPersona(ArrayList<Object> array) {
+    private static void agregarPersona(ArrayList<Object[]> array) {
         System.out.println("--- AGREGAR PERSONA ---");
 
         System.out.println("Ingresa el nombre de la persona: ");
-        String nombre = sc.nextLine();
+        String nombre = sc.next();
 
         System.out.println("Ingresa el apellido de la persona: ");
-        String apellido = sc.nextLine();
+        String apellido = sc.next();
 
         System.out.println("Ingresa el teléfono de la persona: ");
         int telefono = sc.nextInt();
 
         System.out.println("Ingresa el DNI de la persona: ");
-        String dni = sc.nextLine();
+        String dni = sc.next();
 
-        array.add(nombre);
-        array.add(apellido);
-        array.add(telefono);
-        array.add(dni);
+        array.add(new Object[]{nombre, apellido, telefono, dni});
     }
 
-    private static void buscarPersona(ArrayList<Object> array) {
+    private static void buscarPersona(ArrayList<Object[]> array) {
         System.out.println("--- BUSCAR PERSONA ---");
 
         System.out.println("Ingresa el DNI de la persona que quieres buscar: ");
         String dni = sc.nextLine();
-
-        if (array.contains(dni)) {
-            System.out.println("Persona encontrada: "+array.indexOf(dni));
-        }
     }
 
-    private static void borrarPersona(ArrayList<Object> array) {
+    private static void borrarPersona(ArrayList<Object[]> array) {
         System.out.println("--- BORRAR PERSONA ---");
     }
 
-    private static void listarPersonas(ArrayList<Object> array) {
+    private static void listarPersonas(ArrayList<Object[]> array) {
         System.out.println("--- LISTAR PERSONAS ---");
+
+    }
+
+    private static void confirmacion(boolean yesNo) {
+        if (yesNo) {
+            System.out.println("Ejecución realizada CORRECTAMENTE");
+        } else {
+            System.out.println("Ejecución realizada INCORRECTAMENTE");
+        }
     }
 
 }
