343f7e6738a0466a8b87904b82e385034c8ea0d5
Jorge <99258565+Jbergo@users.noreply.github.com>
2026-02-09 13:00:58 +0100

    update

--- MPO/EjerciciosT4/src/Ejercicio9/Main.java
+++ MPO/EjerciciosT4/src/Ejercicio9/Main.java
@@ -8,8 +8,14 @@ devuelva una nueva lista con todos los elementos de ambas listas,
 pero sin duplicados y ordenada de menor a mayor.
  */
 
+import java.util.List;
+import java.util.function.BiFunction;
+
 public class Main {
     public static void main(String[] args) {
 
+        BiFunction<List<Integer>, List<Integer>, List<Integer>> fusionList = (integers, integers2) -> {
+
+        };
     }
 }
